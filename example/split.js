9ecaefdfec3f38478ef27d86740c4b9a758f5f94
prasanth <kesavarapu.siva@gmail.com>
2022-03-01 10:54:40 +0530

    chore: pull react-ace

--- example/split.js
+++ example/split.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import { Component, render, h } from "preact";
 import SplitAceEditor from "../src/split";
 import "ace-builds/src-noconflict/mode-jsx";
 import "ace-builds/src-min-noconflict/ext-searchbox";
